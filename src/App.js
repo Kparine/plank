44202f6ebb1ce99eb4595b9703ff0d207fe1f1f2
Kevin Parine <kparine314@gmail.com>
2019-04-30 12:02:33 -0700

    Favicon BG IMG

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {CircleArrow as ScrollUpButton} from 'react-scroll-up-button';
+
+
 
 function App() {
   return (
     <div className="App">
+    <h1>HOWDY WORLD</h1>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function NavFooter(){
+  return (
+    <div id='NavFooter' className='footer'>
+      <h6>Powered by &lt;strong&gt; Coffee. &lt;/strong&gt;</h6>
+    </div>
+  )
+}
